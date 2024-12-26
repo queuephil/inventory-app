1dc46af8c072d460986399b535d70101cb5769a5
pquauke <philipp.quauke@gmail.com>
2024-12-26 18:31:05 +0100

    Add pool data

--- db/pool.js
+++ db/pool.js
@@ -1,14 +1,7 @@
 const { Pool } = require('pg')
 require('dotenv').config()
 
-const LOCAL_URL =
-  `postgres://${encodeURIComponent(process.env.LOCAL_USER)}` +
-  `:${encodeURIComponent(process.env.LOCAL_PASSWORD)}` +
-  `@${process.env.LOCAL_HOST}` +
-  `:${process.env.LOCAL_PORT}` +
-  `/${process.env.LOCAL_DATABASE}`
-
 module.exports = new Pool({
-  connectionString: process.env.DATABASE_URL || LOCAL_URL,
+  connectionString: process.env.DATABASE_URL || process.env.LOCAL_URL,
   ssl: { rejectUnauthorized: false },
 })
--- db/populate.js
+++ db/populate.js
@@ -19,16 +19,11 @@ VALUES
   ('A Brief History of Time', 'Stephen Hawking', 'Science'),
   ('Pride and Prejudice', 'Jane Austen', 'Romance');
 `
-
-// dotenv variables
 async function main() {
   console.log('seeding...')
   const client = new Client({
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.POPULATE_URL || process.env.LOCAL_URL,
+    ssl: { rejectUnauthorized: false },
   })
   await client.connect()
   await client.query(SQL)
